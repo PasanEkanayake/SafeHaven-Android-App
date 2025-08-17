8b6cf27c7f76c17643421dda3e21317e4a0245ef
Pasan Ekanayake <pasanekanayake@gmail.com>
2025-08-17 08:56:16 +0530

    commit

---
+++ app/src/main/java/com/example/safehaven/introduction.java
@@ -0,0 +1,40 @@
+package com.example.safehaven;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_introduction);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button backButton = findViewById(R.id.backButton);
+        Button finishButton = findViewById(R.id.finishButton);
+
+        // Back button → go to previous page
+        backButton.setOnClickListener(v -> onBackPressed());
+
+        // Finish button → go to HomeActivity
+        finishButton.setOnClickListener(v -> {
+            Intent intent = new Intent(introduction.this, Home.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+    }
+}
